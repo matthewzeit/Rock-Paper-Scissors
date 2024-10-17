2b7a48b78ae6107e989f8ff50e1fcb856d7321f0
Matt <matt.zeitlinger@yahoo.com>
2024-10-16 20:12:06 -0400

    created a play game function (5 rounds) and encapsulated code into this single function.

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,64 +1,54 @@
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    
+    let humanScore = 0;
+    let computerScore = 0;
 
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 100);
-    if (computerChoice <= 33) {
-        computerChoice = 'Rock';
-    } else if (computerChoice <= 66) {
-        computerChoice = 'Paper'
-    } else {
-        computerChoice = 'Scissors'
+    function getComputerChoice() {
+        let computerChoice = Math.floor(Math.random() * 100);
+        if (computerChoice <= 33) {
+            computerChoice = 'Rock';
+        } else if (computerChoice <= 66) {
+            computerChoice = 'Paper'
+        } else {
+            computerChoice = 'Scissors'
+        }
+        return computerChoice;
     }
-    return computerChoice;
-}
-
-function getHumanChoice() {
-    let humanChoice = prompt("Please enter your choice 'Rock', 'Paper' or 'Scissors'.", "Rock");
-    if (humanChoice == "Rock" || humanChoice == "Paper" || humanChoice == "Scissors" || humanChoice == "scissors" || humanChoice == "rock" || humanChoice == "paper") {
+    
+    function getHumanChoice() {
+        let humanChoice = prompt("Please enter your choice 'Rock', 'Paper' or 'Scissors'.", "Rock");
         return humanChoice;
-    } else {
-        alert("Your choice was invalid");
+    }
+
+    function playRound() {
+        let userChoice = getHumanChoice().toLowerCase();
+        let computerChoice = getComputerChoice().toLowerCase();
+        if (userChoice == computerChoice) {
+            alert(`The computer chose ${computerChoice} and you chose ${userChoice} the result is a tie!`);
+        } else if (userChoice == 'rock' && computerChoice == 'paper') {
+            alert(`The computer chose ${computerChoice} and you chose ${userChoice} the computer won!`);
+            computerScore++;
+        } else if (userChoice == 'rock' && computerChoice == 'scissors') {
+            alert(`The computer chose ${computerChoice} and you chose ${userChoice} you have won!`);
+            humanScore++;
+        } else if (userChoice == 'paper' && computerChoice == 'rock') {
+            alert(`The computer chose ${computerChoice} and you chose ${userChoice} you have won!`);
+            humanScore++;
+        } else if (userChoice == 'paper' && computerChoice == 'scissors') {
+            alert(`The computer chose ${computerChoice} and you chose ${userChoice} the computer won!`);
+            computerScore++;
+        } else if (userChoice == 'scissors' && computerChoice == 'paper') {
+            alert(`The computer chose ${computerChoice} and you chose ${userChoice} you have won!`);
+             humanScore++
+        } else if (userChoice == 'scissors' && computerChoice == 'rock') {
+            alert(`The computer chose ${computerChoice} and you chose ${userChoice} the computer won!`);
+            computerScore++;
+        }
+    }
+
+    for (let i = 5; (i > humanScore) && (i > computerScore); i * 1) {
+        playRound();
     }
 }
 
-function tryAgain() {
-    let tryAgainChoice = prompt("Would you like to try again? (Yes or No)", "Yes");
-    if (tryAgainChoice == "Yes" || tryAgainChoice == "yes") {
-        tryAgainChoice = getHumanChoice();
-        return tryAgainChoice;
-    }
-}
-
-function playRound() {
-    let userChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    if (userChoice == computerChoice) {
-        alert(`The computer chose ${computerChoice} and you chose ${userChoice} the result is a tie!`);
-    } else if (userChoice == 'Rock' && computerChoice == 'Paper') {
-        alert(`The computer chose ${computerChoice} and you chose ${userChoice} the computer won!`);
-        computerScore++;
-    } else if (userChoice == 'Rock' && computerChoice == 'Scissors') {
-        alert(`The computer chose ${computerChoice} and you chose ${userChoice} you have won!`);
-        humanScore++;
-    } else if (userChoice == 'Paper' && computerChoice == 'Rock') {
-        alert(`The computer chose ${computerChoice} and you chose ${userChoice} you have won!`);
-        humanScore++;
-    } else if (userChoice == 'Paper' && computerChoice == 'Scissors') {
-        alert(`The computer chose ${computerChoice} and you chose ${userChoice} the computer won!`);
-        computerScore++;
-    } else if (userChoice == 'Scissors' && computerChoice == 'Paper') {
-        alert(`The computer chose ${computerChoice} and you chose ${userChoice} you have won!`);
-         humanScore++
-    } else if (userChoice == 'Scissors' && computerChoice == 'Rock') {
-        alert(`The computer chose ${computerChoice} and you chose ${userChoice} the computer won!`);
-        computerScore++;
-    }
-}
-
-playRound();
-
-
-
-console.log(humanScore);
-console.log(computerScore);
+playGame();
